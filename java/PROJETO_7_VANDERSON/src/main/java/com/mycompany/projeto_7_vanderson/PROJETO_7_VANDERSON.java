2f0df1acf357c326e21390d35f09f814d59ba4ee
VANDERSON DA SILVA <v.silva@labcetec.net>
2024-08-09 19:43:01 -0300

    Java Projeto 7 Media + IF

---
+++ java/PROJETO_7_VANDERSON/src/main/java/com/mycompany/projeto_7_vanderson/PROJETO_7_VANDERSON.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_7_vanderson;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author v.silva
+ */
+public class PROJETO_7_VANDERSON {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+                
+        System.out.println("Entre com a Primeira nota : ");
+        int n1 = ler.nextInt();
+        System.out.println("Entre com a Segunda nota : ");
+        int n2 = ler.nextInt();
+        System.out.println("Entre com a Terceira nota : ");
+        int n3 = ler.nextInt();
+        System.out.println("Entre com a Frequencia do aluno : ");
+        int freq = ler.nextInt();
+        
+        if (freq>=75){
+            if ( ((n1+n2+n3)/3)>=7 ){
+                System.out.println("Aluno aprovado!!");
+                System.out.println("Mediaç = " + ((n1+n2+n3)/3));
+            }
+            else{
+                System.out.println("Aluno Reprovado!!");
+                System.out.println("Media = " + ((n1+n2+n3)/3));
+            }
+        }
+        else {
+            System.out.println("Aluno Reprovado por falta!!!");
+        }
+        
+        
+
+    }
+}
