40db687d352c31f78a9b55c7f1bfe063b6b6329f
kallangos <vandersondasilva@gmail.com>
2024-08-11 23:00:19 -0300

    Projetos 10 e 11

---
+++ java/PROJETO_11_VANDERSON/src/main/java/com/mycompany/projeto_11_vanderson/PROJETO_11_VANDERSON.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_11_vanderson;
+
+/**
+ *
+ * @author Thor
+ */
+public class PROJETO_11_VANDERSON {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
