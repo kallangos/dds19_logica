a4d9a82d590a1a67bab735ed4cb288095a444860
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-08-28 19:44:16 -0300

    Atualizando Git Exercicios em Aula

---
+++ java/PROJETO_12_VANDERSON/src/main/java/com/mycompany/projeto_12_vanderson/PROJETO_12_VANDERSON.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_12_vanderson;
+
+/**
+ *
+ * @author v.silva
+ */
+public class PROJETO_12_VANDERSON {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
