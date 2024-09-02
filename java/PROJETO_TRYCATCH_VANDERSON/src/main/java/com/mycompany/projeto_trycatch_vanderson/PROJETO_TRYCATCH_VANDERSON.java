fcfb90e5f0d7f618a963d28020b0bb94b1c93643
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-09-02 20:04:26 -0300

    Teste TryCatch

---
+++ java/PROJETO_TRYCATCH_VANDERSON/src/main/java/com/mycompany/projeto_trycatch_vanderson/PROJETO_TRYCATCH_VANDERSON.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_trycatch_vanderson;
+import java.util.Scanner;
+
+/**
+ *
+ * @author v.silva
+ */
+public class PROJETO_TRYCATCH_VANDERSON {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        String texto = "";
+        String valorString = "4.2"; // cvalor numeroco como string
+        
+        try {
+            int valorInteiro = Integer.parseInt(valorString);
+            System.out.println("Valor Inteiro: " + valorInteiro );
+        } catch  (NumberFormatException Erro01){
+            System.out.println(" Erro ao converter : " + Erro01.getMessage());
+        }
+        
+        
+    }
+}
