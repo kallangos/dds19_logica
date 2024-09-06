0b78670bf3d7f8ba992e67215e5dfb23ad9f5364
kallangos <vandersondasilva@gmail.com>
2024-09-06 18:04:01 -0300

    PROJETO 14 - DATA INPUT STREAM

--- java/PROJETO_14_VANDERSON/src/main/java/com/mycompany/projeto_14_vanderson/PROJETO_14_VANDERSON.java
+++ java/PROJETO_14_VANDERSON/src/main/java/com/mycompany/projeto_14_vanderson/PROJETO_14_VANDERSON.java
@@ -16,25 +16,32 @@ import java.util.Random;
 public class PROJETO_14_VANDERSON {
 
     public static void main(String[] args) throws IOException{
+        Random nAleatorio = new Random();
+        DataInputStream dado;
         String s = "";
+        double raizQuadrada = 0;
+        int d = 0 ;
         int linha = 5; int coluna = 5;
         int matriz5x5[][] = new int[linha][coluna];
         int matriz25[]= new int[linha*coluna];
-        DataInputStream dado;
-        int d = 0 ;
+        int matriz25countRep[]= new int[linha*coluna];
+        int somaLinhas[]= new int[linha];
+        int somaColunas[]= new int[coluna];
         int somaDiagonalPrincipal = 0 ;int somaDiagonaSecundaria = 0 ;
-        Random nAleatorio = new Random();
+        
         
 
         for (int l =0 ; l<=linha-1 ; l++){
 
             for (int c =0 ; c<=coluna-1 ; c++){
                 try {
-                    // System.out.println("Entre com o numero [" + (l+1) + "] [" + (c+1) + "] da lista :");
-                    // dado = new DataInputStream(System.in); //Inicia para pegar inforção do teclado.
-                    // s = dado.readLine(); // lê uma linha do teclado
-                    // matriz5x5[l][c] = Integer.parseInt(s);//converte de string para float
-                    matriz5x5[l][c] = nAleatorio.nextInt(30)+1;
+                    
+                    System.out.println("Entre com o numero [" + (l+1) + "] [" + (c+1) + "] da lista :");
+                    dado = new DataInputStream(System.in); //Inicia para pegar inforção do teclado.
+                    s = dado.readLine(); // lê uma linha do teclado
+                    matriz5x5[l][c] = Integer.parseInt(s);//converte de string para float
+                    
+                    //matriz5x5[l][c] = nAleatorio.nextInt(30)+1;
                 } catch (InputMismatchException Erro01) {
                     System.out.println("Nao foi inserido um numero inteiro!");
                     System.out.println("Erro: " + Erro01);
@@ -46,29 +53,22 @@ public class PROJETO_14_VANDERSON {
 
         }            
 
-        System.out.println("-----------------------------------");
+        System.out.println(" -----------------------------------");
         System.out.println(" Exbindo valores armazenados na Matrix 5x5");
         for (int l =0 ; l<=linha-1 ; l++){
             for (int c =0 ; c<=coluna-1 ; c++){
-                    System.out.print(" " + matriz5x5[l][c] + " ");
+                    //System.out.print(" " + matriz5x5[l][c] + " ");
+                    System.out.printf(" %2d ", matriz5x5[l][c] );
             }
             System.out.println(" ");
         }            
 
-        System.out.println("-----------------------------------");
-        System.out.println(" Calculando valores na diagonal");
-        for (int l =0 ; l<=linha-1 ; l++){
-                somaDiagonalPrincipal+=matriz5x5[l][d];
-                d++; 
-        }            
-        System.out.println(" Exbindo soma na diagonal = " + somaDiagonalPrincipal);
-        
+    
         // transformando matrix em vetor para ordenação.
         d=0;
         for (int l =0 ; l<=linha-1 ; l++){
             for (int c =0 ; c<=coluna-1 ; c++){
                     matriz25[d]=matriz5x5[l][c];
-                    System.out.print(" [" + matriz25[d] + "] ");
                     d++; 
             }
         }  
@@ -83,15 +83,100 @@ public class PROJETO_14_VANDERSON {
                 }
             }
         }
+        // Transportando vetor ordenado para matriz
+        d=0;
+        for (int l =0 ; l<=linha-1 ; l++){
+            for (int c =0 ; c<=coluna-1 ; c++){
+                    matriz5x5[l][c]=matriz25[d];
+                    d++; 
+            }
+        }  
+        // Exibindo Matriz ordenanda
+        System.out.println(" -----------------------------------");
+        System.out.println(" Exibindo matriz ordenada: ");
+        for (int l =0 ; l<=linha-1 ; l++){
+            for (int c =0 ; c<=coluna-1 ; c++){
+                    //System.out.print(" " + matriz5x5[l][c] + " ");
+                    System.out.printf(" %2d ", matriz5x5[l][c] );
+            }
+            System.out.println(" ");
+        } 
+        
+        // Calculando a soma de cada coluna e linha
+        System.out.println(" -----------------------------------");
+        System.out.println(" Exibindo a soma das colunas e linhas: ");
 
-        System.out.println("-----------------------------------");
-        System.out.println(" Exibindo vetor ordenado: ");
-        for (int i = 0 ; i < matriz25.length ; i++ ){
-            if ( (i%coluna)==0 ){
-                System.out.println("");
+        for (int l =0 ; l<=linha-1 ; l++){
+            for (int c =0 ; c<=coluna-1 ; c++){
+
+                    //System.out.printf(" %2d ", matriz5x5[l][c] );
+                    somaColunas[c]+=matriz5x5[l][c];
+                    somaLinhas[l]+=matriz5x5[l][c];
             }
-            System.out.print( " [" + matriz25[i] + "] ");
+            //System.out.println(" ");
+        } 
+        for (int l = 0 ; l<=linha-1 ; l++){
+            System.out.printf(" Sonha da linha [" + l + "] = %2d \n", somaLinhas[l] );
+        }
+        for (int c = 0 ; c<=coluna-1 ; c++){
+            System.out.printf(" Sonha da Coluna [" + c + "] = %2d \n", somaColunas[c] );
+        }
+        
+        
+        // Contando quais numeros repetiram
+        for (int i = 0 ; i<=(linha*coluna-1) ; i++){
+
+            for (int j = 0 ; j<=(linha*coluna-1) ; j++){
+                if (matriz25[i]==matriz25[j]){
+                    matriz25countRep[i]+=1;
+                }
             }
+        }
+        // Exibindo na tela quantas vezes um numero se repetiu
+        System.out.println(" -----------------------------------");
+        System.out.println(" Exibindo o numero de vezes que um numero apareceu: ");
+
+        for (int i = 1 ; i<=(linha*coluna-1) ; i++){
+        //    if ( (matriz25countRep[i-1]>1) & (matriz25countRep[i]!=matriz25countRep[i-1])){
+            if ( (matriz25countRep[i]!=matriz25countRep[i-1]) & (matriz25countRep[i-1]>1)){
+                System.out.printf(" O numero %2d se repetiu %2d vezes . \n", matriz25[i-1], matriz25countRep[i-1] );    
+            }
+        }
+
+        // Calculando a soma dos valores nas diagonais
+        d=0;
+        System.out.println(" -----------------------------------");
+        System.out.println(" Calculando valores nas diagonais");
+        for (int l =0 ; l<=linha-1 ; l++){
+                somaDiagonalPrincipal+=matriz5x5[l][d];
+                somaDiagonaSecundaria+=matriz5x5[4-l][d];
+                d++; 
+        }            
+        System.out.println(" Exbindo soma na diagonal = " + somaDiagonalPrincipal);
+        System.out.println(" Exbindo soma na diagonal = " + somaDiagonaSecundaria);
+        
+        // verificando se os numeros são primos e alterando na mesma posiçao da matrix por -1
+        System.out.println(" -----------------------------------");
+        System.out.println(" Substituindo numeros primos por -1");
+        for (int l =0 ; l<=linha-1 ; l++){
+            for (int c =0 ; c<=coluna-1 ; c++){
+                
+                raizQuadrada = Math.sqrt(matriz5x5[l][c]);
+                d=0;
+                
+                for (int i = 1; i <= matriz5x5[l][c]; i++) {
+                    if (matriz5x5[l][c] % i == 0){
+                        d++;
+                    } else if ((matriz5x5[l][c] % i != 0) & (d==1)) {
+                        matriz5x5[l][c]=-1;
+                    } 
+                    
+                }
+                    System.out.printf(" %2d ", matriz5x5[l][c] );
+            }
+            System.out.println(" ");
+        } 
+        
         }
         
         
