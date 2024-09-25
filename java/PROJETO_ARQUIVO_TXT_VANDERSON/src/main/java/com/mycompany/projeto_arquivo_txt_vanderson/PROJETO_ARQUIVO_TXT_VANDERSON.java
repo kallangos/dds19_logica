c3a9453a7b80cd6057593faa5b7ae594bf98b47e
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-09-25 18:57:08 -0300

    PROJETO_ARQUIVO_TXT_VANDERSON

---
+++ java/PROJETO_ARQUIVO_TXT_VANDERSON/src/main/java/com/mycompany/projeto_arquivo_txt_vanderson/PROJETO_ARQUIVO_TXT_VANDERSON.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_arquivo_txt_vanderson;
+
+/**
+ *
+ * @author v.silva
+ */
+public class PROJETO_ARQUIVO_TXT_VANDERSON {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
