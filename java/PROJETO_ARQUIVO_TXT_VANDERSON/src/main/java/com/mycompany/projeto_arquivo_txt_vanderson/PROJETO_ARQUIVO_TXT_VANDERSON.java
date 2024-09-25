83ce3995234bfd918eaba2af90958ac2c684d752
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-09-25 19:15:51 -0300

    PROJETO_ARQUIVO_TXT_VANDERSON

--- java/PROJETO_ARQUIVO_TXT_VANDERSON/src/main/java/com/mycompany/projeto_arquivo_txt_vanderson/PROJETO_ARQUIVO_TXT_VANDERSON.java
+++ java/PROJETO_ARQUIVO_TXT_VANDERSON/src/main/java/com/mycompany/projeto_arquivo_txt_vanderson/PROJETO_ARQUIVO_TXT_VANDERSON.java
@@ -4,13 +4,41 @@
 
 package com.mycompany.projeto_arquivo_txt_vanderson;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  *
  * @author v.silva
  */
 public class PROJETO_ARQUIVO_TXT_VANDERSON {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        String nomeArquivo = "";
+        Date date = new Date();
+        int valorUsuario = 0 ;
+        
+        
+        System.out.print("\nInforme nome do arquivo : ");
+        nomeArquivo = sc.nextLine();
+        System.out.print("\nInforme um valor : ");
+        valorUsuario = sc.nextInt();
+        sc.nextLine();
+        
+        FileWriter arquivo = new FileWriter(".\\" + nomeArquivo + ".txt");
+        PrintWriter gravar = new PrintWriter(arquivo);
+        
+        gravar.printf("Tabuada do " + valorUsuario + " | " + date + "\n");
+        gravar.printf("------------------------");
+        for (int i = 1 ; i<=10 ; i++){
+            gravar.printf("\n" + i + "x" + valorUsuario + " = " + (i*valorUsuario));
+        }
+        
+        gravar.printf("\n------------------------");
+        arquivo.close();
     }
 }
