bb7aa00f85c669c590c3c17f4a7c5549fa863a17
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-09-25 21:38:55 -0300

    EXERCICIO_03_REVISAO

---
+++ java/Lista de Exercícios 1 - Revisao/EXERCICIO_03_REVISAO/src/main/java/com/mycompany/exercicio_03_revisao/EXERCICIO_03_REVISAO.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio_03_revisao;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+    Escreva um programa que leia dois números inteiros e apresente na tela sua soma apenas se ambos tiverem
+o mesmo sinal (positivo ou negativo). Use o mesmo formato do exercício anterior e, caso os números
+fornecidos tenham sinais trocados o programa deve escrever na tela que os "Dados de Entrada são
+Inválidos".
+ */
+public class EXERCICIO_03_REVISAO {
+
+    public static void main(String[] args) throws IOException{
+
+        Scanner sc = new Scanner(System.in);
+        int numUm, numDois = 0;
+        
+        System.out.printf("-------------------------------------------\n");
+        System.out.print("Foneça o primeiro numero INTEIRO: ");
+        numUm = sc.nextInt();
+        
+        System.out.print("Foneça o segundo numero INTEIRO: ");
+        numDois = sc.nextInt();
+        
+        if ((numUm>=0)&&(numDois>=0)){
+            System.out.printf("-------------------------------------------\n");
+            System.out.printf("Soma de " + numUm + " com " + numDois + " = " + (numUm+numDois));    
+        } else if ((numUm<0)&&(numDois<0)) {
+            System.out.printf("-------------------------------------------\n");
+            System.out.printf("Soma de " + numUm + " com " + numDois + " = " + (numUm+numDois));    
+        } else {
+            System.out.printf("-------------------------------------------\n");
+            System.out.printf("Para realizar a somar,\n");
+            System.out.printf("os dois numeros devem ter o mesmo sinal!\n");
+            System.out.printf("Saindo...\n");
+        }
+
+
+        
+    }
+}
