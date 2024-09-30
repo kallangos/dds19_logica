f7e63b4383cf6c05a6de670f7cf09b60dd895b52
kallangos <vandersondasilva@gmail.com>
2024-09-29 23:02:44 -0300

    EXERCICIO_14_REVISAOe

--- java/Lista de Exercícios 1 - Revisao/EXERCICIO_14_REVISAO/src/main/java/com/mycompany/exercicio_14_revisao/EXERCICIO_14_REVISAO.java
+++ java/Lista de Exercícios 1 - Revisao/EXERCICIO_14_REVISAO/src/main/java/com/mycompany/exercicio_14_revisao/EXERCICIO_14_REVISAO.java
@@ -18,42 +18,71 @@ public class EXERCICIO_14_REVISAO {
             try {
                 resultado=ler.nextLine();
             } catch (InputMismatchException Erro01) {
-                System.out.println("Opção INVALIDA!");
-                System.out.println("Nao foi inserido um numero INTEIRO!");
+                System.out.println("INVALIDO!");
+                System.out.println("Nao foi reconhecido o texto");
                 System.out.println("Erro: " + Erro01);
                 System.exit(0);
             }
         return resultado ;
     }    
-    public static int lerSalario(){
+    public static float lerSalario(){
         Scanner ler = new Scanner(System.in);
-        int resultado = 0;
+        float resultado = 0;
             try {
-                resultado=ler.nextInt();
+                resultado=ler.nextFloat();
             } catch (InputMismatchException Erro01) {
                 System.out.println("Opção INVALIDA!");
-                System.out.println("Nao foi inserido um numero INTEIRO!");
+                System.out.println("Deve ser um numero FLOAT");
+                System.out.println("ou INTERIRO");
                 System.out.println("Erro: " + Erro01);
                 System.exit(0);
             }
         return resultado ;
     }
     public static void main(String[] args) {
-        boolean sair = true;
-        float somaSalariosAtuais, somaSalarioReagustados;
+        boolean sair = false;
+        float somaSalariosAtuais = 0, somaSalarioReagustados = 0, salarioAtual = 0, salarioReajustado = 0;
         Scanner ler = new Scanner(System.in);
-        String nomeFuncionario;
+        String nomeFuncionario = "";
         
+        char[] nomeFuncionarioZ = new char[40];
         
-        while (sair!=false){
+        while (sair!=true){
             System.out.print("Entre com o nome do funcionario: ");
             nomeFuncionario = lerString();
-            System.out.println("|  "  + nomeFuncionario.toUpperCase());
-            if (nomeFuncionario.toUpperCase()=="FIM"){
-            sair=true;
-            System.out.println("SAI AQUI");
+            if (nomeFuncionario.equalsIgnoreCase("fim")){
+                sair = true;
+                System.out.println("Saindo...");
             }
+           
+
+            
+            if (sair != true){
+                System.out.print("Entre com salario: ");
+                salarioAtual = lerSalario();
+                    if (salarioAtual<=150.0){
+                        salarioReajustado = (float) (salarioAtual * 1.25);
+                        } else if ((salarioAtual>150.0)&&(salarioAtual<=300.0)){
+                                salarioReajustado = (float) (salarioAtual * 1.2);
+                            } else if ((salarioAtual>300.0)&&(salarioAtual<=600.0)){
+                                    salarioReajustado = (float) (salarioAtual * 1.15);
+                                } else if (salarioAtual>600.0){
+                                        salarioReajustado = (float) (salarioAtual * 1.10);
+                    }
+                    System.out.printf("-----------------------------------------------\n");                    
+                    System.out.println("FUNCIONARIO : " + nomeFuncionario );
+                    System.out.printf("SALARIO REAJUSTADO : R$ %,10.2f \n",salarioReajustado );
+                    somaSalariosAtuais+=salarioAtual;
+                    somaSalarioReagustados+=salarioReajustado;
+            }
+            
+
         }
-        System.out.println("SAI");
+        System.out.printf("-----------------------------------------------\n");
+        System.out.printf("SOMA DOS SALARIOs REAJUSTADOs : R$ %,10.2f \n",somaSalariosAtuais );
+        System.out.printf("SOMA DOS SALARIOs REAJUSTADOs : R$ %,10.2f \n",somaSalarioReagustados );
+        System.out.printf("Saindo...\n");
+
+        
     }
 }
