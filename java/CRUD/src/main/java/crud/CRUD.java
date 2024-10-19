e7c5e5a481c1bebf504e1da9d949355005340f6a
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-10-19 12:23:32 -0300

    CRUD 1

---
+++ java/CRUD/src/main/java/crud/CRUD.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package crud;
+
+/**
+ *
+ * @author v.silva
+ */
+public class CRUD {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        CRUDFORM crud = new CRUDFORM();
+        crud.setVisible(true);
+        // this.dispose;
+    }
+}
