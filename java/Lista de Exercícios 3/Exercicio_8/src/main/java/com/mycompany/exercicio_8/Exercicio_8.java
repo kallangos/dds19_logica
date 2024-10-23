9a083398959c687da4b38281aaa9786e2bf9ae7a
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-10-23 20:41:14 -0300

    Lista de Exercícios 3

--- java/Lista de Exercícios 3/Exercicio_8/src/main/java/com/mycompany/exercicio_8/Exercicio_8.java
+++ java/Lista de Exercícios 3/Exercicio_8/src/main/java/com/mycompany/exercicio_8/Exercicio_8.java
@@ -4,13 +4,33 @@
 
 package com.mycompany.exercicio_8;
 
+import java.util.Scanner;
+
 /**
  *
  * @author v.silva
+ * Média de Notas de Alunos
+ * O sistema educacional em que você está trabalhando precisa calcular a média de um aluno e informar 
+ * se ele foi aprovado ou reprovado.
  */
 public class Exercicio_8 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int nAval = 0;
+        float media = 0, somaNotas = 0 ;
+        
+        System.out.print("Informe quantas notas este aluno teve ? :  " );
+        nAval = sc.nextInt();
+        int vetorNotas[] = new int[nAval];
+        
+        for (int i = 0 ; i>=vetorNotas.length-1; i++){
+            System.out.print("Entre com a " + (i+1) + "º nota :  " );
+            vetorNotas[i] = sc.nextInt();
+            somaNotas=+vetorNotas[i];
+        }
+        
+        media = somaNotas/vetorNotas.length-1;
+        System.out.println("Media = " + media);
     }
 }
