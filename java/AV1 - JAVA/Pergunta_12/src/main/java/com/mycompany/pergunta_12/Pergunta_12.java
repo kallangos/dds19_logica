bc324e646bf0428a2cd53f10ba9d84998ba6923f
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-10-25 21:27:24 -0300

    Pergunta 06

--- java/AV1 - JAVA/Pergunta_12/src/main/java/com/mycompany/pergunta_12/Pergunta_12.java
+++ java/AV1 - JAVA/Pergunta_12/src/main/java/com/mycompany/pergunta_12/Pergunta_12.java
@@ -13,34 +13,38 @@ import javax.swing.JOptionPane;
 public class Pergunta_12 {
 
     public static void main(String[] args) {
-        int [] dadosInsert = new int[3];
+        int [] dadosInsert = new int[10];
         String recebeDado = "";
-        int qtd = 0;
         int copia = 0;
         System.out.println("Hello World!");
+        int novoNum = 0 ;
         
         for (int i = 0 ; i<dadosInsert.length ; i++){
             recebeDado = JOptionPane.showInputDialog(null, "Entre com o "+ (i+1) + "º numero inteiro: ", "", JOptionPane.INFORMATION_MESSAGE);
             dadosInsert[i]=Integer.parseInt(recebeDado);
         }
-        
-         for (int J = 0 ; J<=dadosInsert.length-1 ; J++){
-             System.out.println("J="+ J);    
-              for (int i = 0 ; i<=dadosInsert.length-1 ; i++){
-             System.out.println("i="+ i);    
-                  if (dadosInsert[J]>dadosInsert[J+1]){
-                              copia =dadosInsert [i];
-                              dadosInsert[i]=dadosInsert[i+1];
-                              dadosInsert[i+1]= copia;
-                  }
-              }
-          }
-         
-         System.out.println("ordenando");    
-         for (int i = 0 ; i<dadosInsert.length-1 ; i++){
+
+        for (int J = 0 ; J <dadosInsert.length ; J++) {
+                for (int i = 0 ; i <dadosInsert.length-1 ; i++){
+                        if (dadosInsert[i]>dadosInsert[i+1]){
+                                copia= dadosInsert[i];
+                                dadosInsert[i]= dadosInsert[i+1];
+                                dadosInsert[i+1] = copia;
+                        }
+                }
+        }
+
+         System.out.println("vetor ordenando");    
+         for (int i = 0 ; i<dadosInsert.length ; i++){
             System.out.println(dadosInsert[i]);    
          }
          
+         recebeDado = JOptionPane.showInputDialog(null, "Entre com um numero inteiro: ", "", JOptionPane.INFORMATION_MESSAGE);
+         novoNum = Integer.parseInt(recebeDado);
+         for (int i = 0 ; i <dadosInsert.length-1 ; i++){
+             
+         }
+
          
         //JOptionPane.showMessageDialog(null, idade + "anos, " + (dias/30)+ "meses, " + dias + "dias.", "", JOptionPane.INFORMATION_MESSAGE);
         
