c58f09bb19ef0458de6efdc83975e228ce6c891d
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-11-06 21:19:44 -0300

    PROJETO_18_VANDERSON_2

---
+++ java/PROJETO_18_VANDERSON_2/src/main/java/com/mycompany/projeto_18_vanderson_2/PROJETO_18_VANDERSON_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_18_vanderson_2;
+
+/**
+ *
+ * @author v.silva
+ */
+public class PROJETO_18_VANDERSON_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
